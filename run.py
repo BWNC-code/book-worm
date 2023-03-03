cbccce5c00c875f4b8330438f3e431190a3f60ec
BWNC-code <bryn.wheeler@gmail.com>
2023-03-03 22:15:14 +0000

    Add add_book function

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-#define scope
+# define scope
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -10,12 +10,30 @@ SCOPE = [
     ]
 
 # add credentials to the account
-CREDS = Credentials.from_json_keyfile_name('creds.json')
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 
 # authorize the clientsheet
-CLIENT = gspread.authorize(creds)
+CLIENT = gspread.authorize(SCOPED_CREDS)
 
 # get the instance of the Spreadsheet
-SHEET = client.open('library').sheet1
+SHEET = CLIENT.open('book_worm').sheet1
 
+# define the add_book function
+def add_book():
+    """
+    Add new book to database
+    """
+    print("Enter the book details below:")
+    title = input("Title: ")
+    author = input("Author: ")
+    year = input("Year (optional): ")
+    genre = input("Genre: ")
+    # add input validation here
+    
+    # add the book to the sheet
+    SHEET.append_row([title, author, year, genre])
+    print("Book added successfully!")
+
+
+add_book()
